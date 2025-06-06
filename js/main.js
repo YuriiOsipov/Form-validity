e12a1f0834558454b2dd15ce2e94fbaab4d6d5a2
Yurii Osipov <jeetforskill@gmail.com>
2025-06-06 21:31:17 +0300

    Redact

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import CheckboxAgree from "./components/Checkbox-agree.js";
-// import FormsValidation from "./components/FormsValidation.js";
+
 import FormValidation from "./components/FormValidation.js";
 
 new CheckboxAgree();
-// new FormsValidation();
+
 new FormValidation();
